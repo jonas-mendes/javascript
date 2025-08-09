8b427b5a8cb471e0899cce1ff3c5221607174a43
jxmend <jonas.msilva.jms@gmail.com>
2025-08-08 21:07:53 -0300

    estudos 08.08

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 60.5
+console.log(`A velocidadde do seu carro eh ${vel}Km/h`)
+if(vel > 60) {
+    console.log(`voce ultrapassou a velocidade permitido. Multado!`)
+}
+
+if(vel <=60) {
+    console.log(`Voce esta na velocidade permitida, dirija com cuidado.`)
+}
+console.log(`Dirija  sempre usando cinto de seguranca!`)
---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 22
+
+if (idade > 18) {
+    console.log(`Vota`)
+} else {
+    if (idade >= 16 && idade < 18){
+        console.log ('Voto opcional')
+    }
+    console.log(`Nao vota`)
+}
